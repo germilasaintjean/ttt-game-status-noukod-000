de52a8063dacd53b613c9425d65caafda7f01b79
Saint-Jean Germila <germila@noukod.com>
2018-09-04 14:11:42 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,9 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+         WIN_COMBINATIONS = [0,1,2],
+                            [3,4,5],
+                            [6,7,8]
+                            
+                            
 # Define your WIN_COMBINATIONS constant
