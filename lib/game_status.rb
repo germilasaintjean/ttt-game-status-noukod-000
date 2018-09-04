0c058ca1898cba930172054280fb3da98fb8e635
Saint-Jean Germila <germila@noukod.com>
2018-09-04 15:12:18 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,6 +28,5 @@ end
   if (position_1 == "X" && position_2 == "X" position_3 == "X" || position_1 == "O" && position_2 == "O" position_3 == "O")
    return win_combination
  end
- return false 
+ return false
 end
-     
