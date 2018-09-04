53118792d03b85e46a9ac5b308e19d06bbb48dc4
Saint-Jean Germila <germila@noukod.com>
2018-09-04 15:05:01 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,14 +5,16 @@ end
 
 def won_board board
 
-        WIN_COMBINATIONS = [[0,1,2],
+        WIN_COMBINATIONS = [
+                           [0,1,2],
                            [3,4,5],
                            [6,7,8],
                            [0,3,6],
                            [1,4,7],
                            [2,5,8],
                            [0,4,8],
-                           [6,4,2]]
+                           [6,4,2]
+                         ]
 
         win_combination.each do |win_combination|
         win_index_1 =  win_combination[0]
